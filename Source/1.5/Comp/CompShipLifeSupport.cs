5b379e7f1e4b61d417420095d11261594ae9aea1
KentHaeger <kent.haeger@gmail.com>
2024-05-17 15:02:31 -0500

    Initial commit

---
+++ Source/1.5/Comp/CompShipLifeSupport.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace SaveOurShip2
+{
+	public class CompShipLifeSupport : ThingComp
+	{
+		public bool active = false;
+		CompPowerTrader powerComp;
+		CompFlickable flickComp;
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+		{
+			base.PostSpawnSetup(respawningAfterLoad);
+			powerComp = parent.TryGetComp<CompPowerTrader>();
+			flickComp = parent.TryGetComp<CompFlickable>();
+			if (powerComp.PowerOn && flickComp.SwitchIsOn)
+				active = true;
+			//Log.Message("Spawned LS: " + this.parent + " on map: " + this.parent.Map);
+		}
+		public override void CompTick()
+		{
+			base.CompTick();
+			if (Find.TickManager.TicksGame % 360 == 0)
+			{
+				if (powerComp.PowerOn && flickComp.SwitchIsOn)
+					active = true;
+				else
+					active = false;
+			}
+		}
+		public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Values.Look<bool>(ref active, "active", false);
+		}
+	}
+}
---
+++ Source/1.5/Dialog/Dialog_LoadShipDef.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Verse;
+using RimWorld;
+
+namespace SaveOurShip2
+{
+	public class Dialog_LoadShipDef : Dialog_RenameShip
+	{
+		private string ship = "shipdeftoload";
+		private Map Map;
+		public Dialog_LoadShipDef(string ship, Map map)
+		{
+			curName = ship;
+			Map = map;
+		}
+		protected override void SetName(string name)
+		{
+			if (name == ship || string.IsNullOrEmpty(name))
+				return;
+			if (DefDatabase<ShipDef>.GetNamedSilentFail(name) == null)
+			{
+				Log.Error("Ship not found in database: " + name);
+				return;
+			}
+			AttackableShip shipa = new AttackableShip();
+			shipa.attackableShip = DefDatabase<ShipDef>.GetNamed(name);
+			if (shipa.attackableShip.navyExclusive)
+			{
+				shipa.spaceNavyDef = DefDatabase<NavyDef>.AllDefs.Where(n => n.spaceShipDefs.Contains(shipa.attackableShip)).RandomElement();
+				shipa.shipFaction = Find.FactionManager.AllFactions.Where(f => shipa.spaceNavyDef.factionDefs.Contains(f.def)).RandomElement();
+			}
+			Map.passingShipManager.AddShip(shipa);
+		}
+	}
+}
